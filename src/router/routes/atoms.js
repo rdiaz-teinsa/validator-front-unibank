5dd79dd32e32119dce8f84674d6d8ba4bbe5c92d
rdiaz-teinsa <rdiaz@teinsa-pa.com>
2025-10-02 15:01:08 -0500

    reverted fecha

--- src/router/routes/atoms.js
+++ src/router/routes/atoms.js
@@ -270,7 +270,7 @@ export  default [
     {
         path: '/atoms/reports/USC_AT03_AT21_X_REGION',
         name: 'USC_AT03_AT21_X_REGION',
-        component: () => import('@/views/atoms/reports/USC_AT03_AT21_X_REGION'),
+        component: () => import('@/views/atoms/reports/USC_AT03_AT21_X_REGION.vue'),
         meta: {
             resource: 'ACL',
             action: 'read',
