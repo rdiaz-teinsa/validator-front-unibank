d898b5cf159838831f2a3be44f9493e182b3d277
rdiaz-teinsa <rdiaz@teinsa-pa.com>
2025-10-31 17:39:04 -0500

    Missing idRec Actual on linkrenderer

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,11 @@
-const apiBase = (process.env.VUE_APP_API_BASE || '').replace(/\/+$/, '')
+let apiBase = (process.env.VUE_APP_API_BASE || '').replace(/\/+$/, '')
+
+// If not set in .env, default to subdomain-relative: https://apis.<current-host>
+if (!apiBase && typeof window !== 'undefined' && window.location) {
+  const { protocol, host } = window.location
+  const subdomainHost = host.startsWith('apis.') ? host : `apis.${host}`
+  apiBase = `${protocol}//${subdomainHost}`
+}
 
 export default {
   uri: apiBase,
