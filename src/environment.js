11a93ed218a3a123ab802ca607614ea02472f61c
rdiaz-teinsa <rdiaz@teinsa-pa.com>
2025-11-07 18:00:09 -0500

    Missing idRec Actual on linkrenderer

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,17 @@
+let apiBase = (process.env.VUE_APP_API_BASE || '').replace(/\/+$/, '')
+
+// Default to same-origin with "/apis" path instead of using an "apis." subdomain
+if (!apiBase) {
+  if (typeof window !== 'undefined' && window.location) {
+    const { origin } = window.location
+    apiBase = `${origin}/apis`
+  } else {
+    // Fallback for non-browser contexts
+    apiBase = '/apis'
+  }
+}
+
 export default {
-    uri: 'https://apis.tval.online',
+  uri: apiBase,
   version: '1.1.5',
 }
