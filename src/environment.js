9a7385a3a5c2d24d4ee5b11bf579313069585a5f
rdiaz-teinsa <rdiaz@teinsa-pa.com>
2025-11-09 19:12:12 -0500

    Active Directory auth

--- src/environment.js
+++ src/environment.js
@@ -11,8 +11,8 @@ if (!apiBase) {
   }
 }
 export default {
-  // uri: apiBase,
-   uri: 'https://unibank.tval.online/apis',
+  uri: apiBase,
+   // uri: 'https://unibank.tval.online/apis',
   version: '1.1.5',
 
   // Dev-only: allow using MSAL tokens as app accessToken when backend returns null
