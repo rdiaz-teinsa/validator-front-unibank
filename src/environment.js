587d663aeb4d83a9f1791cc63d073afe407696bb
rdiaz-teinsa <rdiaz@teinsa-pa.com>
2025-11-09 19:31:09 -0500

    Active Directory auth unibank

--- src/environment.js
+++ src/environment.js
@@ -12,17 +12,17 @@ if (!apiBase) {
 }
 export default {
   uri: apiBase,
-   // uri: 'https://unibank.tval.online/apis',
+  // uri: 'https://unibank.tval.online/apis',
   version: '1.1.5',
 
   // Dev-only: allow using MSAL tokens as app accessToken when backend returns null
   // Useful on localhost while backend Azure exchange is not wired.
   devAllowMsalTokenAsAccess: true,
   // Azure AD config (make it easy to switch tenants/apps)
-  azureTenantId: 'cf2a7612-3a84-4aac-a4cf-b029e6178dbe',
-  azureClientId: '8c72f1e3-25f2-4335-bf89-16afbbf24f9e',
+  azureTenantId: 'b9ae648a-1d14-45bd-a2b0-0e196e8a6678',
+  azureClientId: '91fcb762-c31f-48ef-a095-c7b17651c8ae',
   // The Application ID URI of your protected API
-  azureApiAppIdUri: 'api://8c72f1e3-25f2-4335-bf89-16afbbf24f9e',
+  azureApiAppIdUri: 'api://91fcb762-c31f-48ef-a095-c7b17651c8ae',
   // The scope name exposed by your API (e.g., 'user_impersonation')
   azureApiScopeName: 'user_impersonation',
   // Optional: override redirectUri if needed per environment
